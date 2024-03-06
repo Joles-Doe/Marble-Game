a0737b629bea50e5b69b5b0928adb5bbcefde05e
Joles-Doe <105786625+Joles-Doe@users.noreply.github.com>
2024-03-06 17:41:28 +0000

    made progress

--- Marble Game/Assets/Scripts/Rotate Baseplate.cs
+++ Marble Game/Assets/Scripts/Rotate Baseplate.cs
@@ -6,19 +6,19 @@ public class RotateBaseplate : MonoBehaviour
 {
     public Cinemachine.CinemachineVirtualCamera virtualCamera;
 
-    Quaternion camOffset;
     Vector2 mouseDelta;
     Vector2 mouseClamp = new Vector2(0f, 0f);
 
-    Quaternion rot;
+    Quaternion camOffset;
+    Quaternion rotX;
+    Quaternion rotZ;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-
-        rot = Quaternion.identity;
         camOffset = virtualCamera.transform.rotation; 
+        camOffset.x = 0f;
     }
 
     // Update is called once per frame
@@ -27,24 +27,11 @@ public class RotateBaseplate : MonoBehaviour
         mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         mouseClamp = new Vector2(mouseClamp.x + mouseDelta.x, mouseClamp.y + mouseDelta.y);
 
-        if (mouseClamp.x > 20)
-        {
-            mouseClamp.x = 20;
-        }
-        if (mouseClamp.y > 20)
-        {
-            mouseClamp.y = 20;
-        }
+        mouseClamp.x = Mathf.Clamp(mouseClamp.x, -20f, 20f);
+        mouseClamp.y = Mathf.Clamp(mouseClamp.y, -20f, 20f);
 
-        if (mouseClamp.x < -20)
-        {
-            mouseClamp.x = -20;
-        }
-        if (mouseClamp.y < -20)
-        { 
-            mouseClamp.y = -20;
-        }
-
-        transform.rotation = Quaternion.Euler(-mouseClamp.y, transform.rotation.y, -mouseClamp.x);
+        rotX = Quaternion.AngleAxis(mouseClamp.x, camOffset * Vector3.back);
+        rotZ = Quaternion.AngleAxis(mouseClamp.y, camOffset * Vector3.right);
+        transform.rotation = rotX * rotZ * Quaternion.Euler(1f, 0f, 1f);
     }
 }
