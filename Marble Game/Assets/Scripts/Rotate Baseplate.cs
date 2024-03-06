685f97254ca97b27f66959b3b7f5a8a411e03fc2
Joles-Doe <105786625+Joles-Doe@users.noreply.github.com>
2024-03-06 20:57:11 +0000

    Losing my mind
    Hate Quaternions.

--- Marble Game/Assets/Scripts/Rotate Baseplate.cs
+++ Marble Game/Assets/Scripts/Rotate Baseplate.cs
@@ -11,6 +11,7 @@ public class RotateBaseplate : MonoBehaviour
 
     Quaternion camOffset;
     Quaternion rotX;
+    Quaternion rotY;
     Quaternion rotZ;
 
     // Start is called before the first frame update
@@ -31,7 +32,21 @@ public class RotateBaseplate : MonoBehaviour
         mouseClamp.y = Mathf.Clamp(mouseClamp.y, -20f, 20f);
 
         rotX = Quaternion.AngleAxis(mouseClamp.x, camOffset * Vector3.back);
+        rotX.y = 0f;
+        rotY = Quaternion.Euler(1f, 1f, 1f);
         rotZ = Quaternion.AngleAxis(mouseClamp.y, camOffset * Vector3.right);
-        transform.rotation = rotX * rotZ * Quaternion.Euler(1f, 0f, 1f);
+        rotZ.y = 0f;
+
+        transform.rotation = rotX * rotZ;
+
+        Debug.Log("x Rot y axis: " + rotX.y);
+        Debug.Log("z Rot y axis: " + rotZ.y);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("ROTATE!!");
+            rotY *= Quaternion.AngleAxis(20, Vector3.up);
+            //transform.Rotate(0f, 200f, 0f, Space.Self);
+            //transform.rotation *= Quaternion.Euler(1f, 20f, 1f);
+        }
     }
 }
